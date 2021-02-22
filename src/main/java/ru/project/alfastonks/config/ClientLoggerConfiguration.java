29ef20b813fa371706405f39b81e36fddff17562
Fr0z3Nn <luckyjohn057@gmail.com>
2021-02-22 19:55:55 +0300

    Добавлено: Логгер для поиска возникших неполадок, связанных с получением респонса

---
+++ src/main/java/ru/project/alfastonks/config/ClientLoggerConfiguration.java
@@ -0,0 +1,13 @@
+package ru.project.alfastonks.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClientLoggerConfiguration {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.HEADERS;
+    }
+}
